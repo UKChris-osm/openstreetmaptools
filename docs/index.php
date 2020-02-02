40111320026ea0401c163d5f1db41ae0ef1f8b71
UKChris-osm <59393694+UKChris-osm@users.noreply.github.com>
2020-02-02 18:59:05 +0000

    Created new index.php file
    Copied over index.htm as a PHP file (index.php)

---
+++ docs/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html lang="en-gb">
+    <head>
+	<title>Open Street Map Tools</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	<meta name="description" content="Open Street Map tools to help local people map Open Street Map better.">
+	<meta name="keywords"    content="open street map openstreetmap osm mapping">
+	<meta name="author"      content="Chris Haig - haignet.co.uk">
+	<meta name="robots"      content="follow,index">
+	<meta name="viewport"    content="width=device-width">
+
+	<link rel="icon"      href="/favicon.ico" type="image/x-icon">
+	<link rel="canonical" href="http://openstreetmap.haignet.co.uk/index.htm">
+
+	<script type="application/ld+json">
+	    {
+		"@context"	: "http://schema.org",
+		"@type"		: "WebSite", 
+		"name"		: "Open Street Map tools",
+		"author"	: "Chris Haig",
+		"description"	: "A small set of tools that can help you map Open Street Map better.",
+		"url"		: "http://openstreetmap.haignet.co.uk/index.htm"
+	    }
+	</script>
+
+	<style>
+	    body { font-size: 1.2em; }
+	    section { border-bottom: 2px dashed red; }
+	</style>
+    </head>
+    <body>
+	<section>
+	    <h1>Open Street Map (<abbr title="Open Street Map">OSM</abbr>) Tools!</h1>
+
+	    <p>
+	    Tools to help you map Open Street Map better, hosted on <a href="https://github.com">GitHub</a>!
+	    </p>
+	</section>
+	<section>
+	    <h2>Post Office's within the <abbr title="United Kingdom">UK</abbr></h2>
+
+	    <h3><a href="https://osm.mathmos.net/postoffice/" title="Post Office comparison tool">Analysing UK Post Office Data in OpenStreetMap</a></h3>
+
+	    <p>
+	    The above tool takes <a href="https://postoffice.co.uk" title="The Post Office, UK">Post Office</a> data stored by Open Street Map
+	    (<abbr title="Open Street Map">OSM</abbr>) and compares it to an officially released data set from the Post Office, allowing for
+	    the ability to see if data stored by <abbr title="Open Street Map">OSM</abbr> is accurate.
+	    </p>
+
+	    <p>The data made available by the post office includes post office names, addresses and opening hours.</p>
+	</section>
+    </body>
+</html>
